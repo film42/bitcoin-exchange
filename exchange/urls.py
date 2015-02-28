d08c5b5255951e5f9d4d3443f27f49829a143f2e
Chris Belyeu <cbelyeu@verisage.us>
2015-02-27 17:43:50 -0700

    added basic serializers, urls, and views to exchange app for REST api

---
+++ exchange/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+
+from exchange import views
+
+admin.autodiscover()
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'orders', views.OrderViewSet)
+router.register(r'trades', views.TradeViewSet)
+router.register(r'exchanges', views.ExchangeViewSet)
+router.register(r'accounts', views.AccountViewSet)
+
+urlpatterns = patterns('',
+                       url(r'^', include(router.urls)),
+)
--- exchange/views.py
+++ exchange/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from models import *
+from rest_framework import viewsets
+from serializers import OrderSerializer, TradeSerializer, ExchangeSerializer, AccountSerializer
 
 # Create your views here.
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TradeViewSet(viewsets.ModelViewSet):
+    queryset = Trade.objects.all()
+    serializer_class = TradeSerializer
+
+
+class ExchangeViewSet(viewsets.ModelViewSet):
+    queryset = Exchange.objects.all()
+    serializer_class = ExchangeSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Exchange.objects.all()
+    serializer_class = AccountSerializer
+
+
