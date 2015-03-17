abb6c0815f1edf4235139ccc8273fa0107cbd579
Garrett Thornburg <film42@gmail.com>
2015-03-17 17:02:09 -0600

    update settings and views to map to temporary routes

--- bitcoin_exchange/urls.py
+++ bitcoin_exchange/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -10,4 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^exchange/', include('exchange.urls')),
     url(r'^admin/', include(admin.site.urls))
-)
+
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
